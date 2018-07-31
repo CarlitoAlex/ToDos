4b700c9695691a0ab100a41232aa07d0d1ba63b3
Alex Urchenko <alexurchenko91@gmail.com>
2018-07-31 03:08:20 +0300

    try to getPoints

--- src/main/java/com/todomy/example/controller/MyTasksController.java
+++ src/main/java/com/todomy/example/controller/MyTasksController.java
@@ -1,20 +1,32 @@
 package com.todomy.example.controller;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import com.todomy.example.repo.TaskRepo;
 import com.todomy.example.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MyTasksController {
+
     @Autowired
     private TaskRepo taskRepo;
+
+    @Autowired
+    private UserRepo userRepo;
 
     @GetMapping("myTaskPage")
     public String taskList(Model model){
         model.addAttribute("tasks" , taskRepo.findAll());
         return "myTaskPage";
+    }
+
+    @PostMapping("checkPoints")
+    public Long myPoint(@RequestParam String username){
+        return userRepo.findPointsByUsername(username);
     }
 }
